75aea60e161e1d43307398dabb5eae1dc3cc0610
zeeshangengar1 <57321494+zeeshangengar1@users.noreply.github.com>
2022-10-31 16:04:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,86 @@
+import java.util.Date;
+
+//for formatting numbers
+import java.text.NumberFormat;
+
+//for taking input
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //printing 
+
+        // System.out.println("Hello peeps!");
+        // System.out.println("Keep peace!");
+
+
+        //data types
+
+
+        //1. integer
+        // int myAge=28;
+        // int temp=30;
+        // int herAge=myAge;
+        // System.out.println(myAge);
+        // System.out.println(herAge);
+        // System.out.println(temp);
+
+        // long viewsCount=3_627_819_881L;
+        // System.out.println(viewsCount);
+
+        // //2. float
+        // float age1=10.99F;
+        // System.out.println(age1);
+
+        // //3. double
+        // double age2=10.99;
+        // System.out.println(age2);
+
+        // //4. boolean
+        // boolean isTrue=true;
+        // System.out.println(isTrue);
+
+        // //5. byte
+        // byte health=30;
+        // System.out.println(health);
+
+        // //6. char
+        // char firstLetter='A';
+        // System.out.println(firstLetter);
+
+        // //7. reference types
+        // Date now=new Date();
+        // System.out.println(now);
+
+        // //8. Strings
+        // String message="Hello World!";
+        // System.out.println(message.length());
+
+        //consonants
+        // final float PI=3.14F;
+        // System.out.println(PI);
+
+        //formatting
+        // NumberFormat currency=NumberFormat.getCurrencyInstance();
+        // String result=currency.format(1234.891);
+        // System.out.println(result);
+
+        // String result1=NumberFormat.getPercentInstance().format(.12);
+        // System.out.println(result1);
+
+
+        //for taking input
+        // Scanner scanner=new Scanner(System.in);
+        // int value=scanner.nextInt();
+        // System.out.println(value);
+        // String name=scanner.nextLine();
+        // System.out.println(name);
+        // String word=scanner.next();
+        // System.out.println(word);
+
+
+
+
+    }
+}
